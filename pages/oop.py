b03686453d3d78262d451df0699a6690ed6edc3b
Zapiron <125368863+DangerousPotential@users.noreply.github.com>
2024-03-19 14:20:00 +0800

    Update oop.py

--- pages/oop.py
+++ pages/oop.py
@@ -16,5 +16,5 @@ if pt_definitions:
     st.markdown("**class:** **:red[blueprint/template]** for creating objects")
     #st.markdown("**object**: **:red[runtime instantiation]** of a class")
     st.markdown("**encapsulation**: **bundling of data and methods within a class entity;** **:red[private data]** are accessible via **:red[public methods]**")
-    st.markdown("**inheritance**: **ability of a **:red[subclass]** to **:red[adopt data and methods]** from a **:red[superclass]**; **:red[subclass can define its own data and method]**)
+    st.markdown("**inheritance**: **ability of a **:red[subclass]** to **:red[adopt data and methods]** from a **:red[superclass]**; **:red[subclass can define its own data and method]**")
     st.markdown("**polymorphism**: ability to **:red[invoke different methods]** in different classes **:red[using the same name]** at **:red[program runtime]**")
