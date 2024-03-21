cd6916c470652b761b3935e4d959276e5dfe5c38
Computing @ ASRJC <125533758+asrjccp@users.noreply.github.com>
2024-03-21 09:21:28 +0800

    Update cb_relationaldatabase.py

--- cb_relationaldatabase.py
+++ cb_relationaldatabase.py
@@ -3,7 +3,6 @@ from PyPDF2 import PdfReader
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import ElasticVectorSearch, Pinecone, Weaviate, FAISS
-# from apikey import apikey
 from langchain.chains.question_answering import load_qa_chain
 from langchain.llms import OpenAI
 from langchain.chat_models import ChatOpenAI
@@ -33,12 +32,12 @@ embeddings = OpenAIEmbeddings()
 docsearch = FAISS.from_texts(texts, embeddings)
 
 # Chain with LangChain
-llm = ChatOpenAI(temperature=0.0, openai_api_key = st.secrets["OPENAI_API_KEY"])
-chain = load_qa_chain(llm = llm, chain_type="stuff")
+llm = ChatOpenAI(temperature=0.0, openai_api_key=st.secrets["OPENAI_API_KEY"])
+chain = load_qa_chain(llm=llm, chain_type="stuff")
 
 # Ask Questions
 def ask_GPT(question):
     query = question
     docs = docsearch.similarity_search(query)
-    response = chain.run(input_documents = docs, question=query)
+    response = chain.run(input_documents=docs, question=query)
     return response
