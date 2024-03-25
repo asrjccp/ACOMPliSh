96cdf672247cb82e9e627e9cfa14ba931113c770
Computing @ ASRJC <125533758+asrjccp@users.noreply.github.com>
2024-03-25 10:20:46 +0800

    Update NoSQL.py

--- pages/NoSQL.py
+++ pages/NoSQL.py
@@ -19,3 +19,23 @@ if pt_definitions:
     st.markdown("**encapsulation**: **bundling of data and methods within a class entity;** **:red[private data]** are accessible via **:red[public methods]**")
     st.markdown("**inheritance**: **ability of a **:red[subclass]** to **:red[adopt data and methods]** from a **:red[superclass]**; **:red[subclass can define its own data and method]**")
     st.markdown("**polymorphism**: ability to **:red[invoke different methods]** in different classes **:red[using the same name]** at **:red[program runtime]**")
+    
+st.divider()
+
+# Initialise a state to keep track of person response"
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+chat_box = st.chat_input("Enter your question")
+if chat_box:
+    with st.chat_message("user"):
+        st.markdown(chat_box)
+    st.session_state.messages.append({"role": "user", "content": chat_box})
+    response = ask_GPT(chat_box)
+    with st.chat_message("assistant"):
+        st.markdown(response)
+        st.session_state.messages.append({"role": "assistant", "content": response})
